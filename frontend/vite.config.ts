6220b929650280ce19a6d9202fa3d922c97277d8
ynubhav <anubhavdixit35@gmail.com>
2025-09-22 14:12:29 +0530

    ...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+
+// no path, no __dirname
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
